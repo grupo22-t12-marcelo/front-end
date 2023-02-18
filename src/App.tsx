81272f302391be6d62ef79c382cd362cef233cd2
SILVESTRE DAUFENBACH JUNIOR <98761413+silvestrejunior@users.noreply.github.com>
2023-02-17 22:49:26 -0300

    feat: rota para page home

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import RoutesMain from "./routes/RoutesMain";
 import GlobalStyle from "./style/GlobalStyle";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
+      <RoutesMain/>
     </div>
   );
 }
