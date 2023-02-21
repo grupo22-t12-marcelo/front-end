7e3ff4cc4101d2eb5d99afad8abe4f85ee28c590
SILVESTRE DAUFENBACH JUNIOR <98761413+silvestrejunior@users.noreply.github.com>
2023-02-21 18:44:13 -0300

    Feat: Inicio do carrocel

--- src/components/SubHeader/styles.ts
+++ src/components/SubHeader/styles.ts
@@ -6,22 +6,21 @@ export const SubHeaderDiv = styled.div`
     align-items: center;
     justify-content: center;
 
-    width: 100%;
-    height: 380px;
-    
+    height: 420px;
     background-color: var(--brand2);
+
     .info-sem-logar{
         display: flex;
         align-items: center;
         justify-content: space-evenly;
         flex-direction: column;
-        width: 748px;
-        height: 261px;
-        .sub-herder-title{
+        width: 100%;
+        height: 100%;
+                .sub-herder-title{
             font-family: var(--heading-font-family);
-            font-weight: var(--heading-1-700);
-            font-size: 44px;
-            line-height: 56px;
+            font-weight: var(--heading-1-600);
+            font-size: 32px;
+            line-height: 40px;
             text-align: center;
             color: var(--grey10);
         }
@@ -34,16 +33,50 @@ export const SubHeaderDiv = styled.div`
         }
         .div-buttons-sub-header{
             display: flex;
-            gap: 30px;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            gap: 20px;
+            width: 100%;
             .button-sub-header{
                 border: 1.5px solid var(--grey10);
                 border-radius: 4px;
-                width: 180px;
-                padding: 12px 28px;
+                width: 90%;
+                padding: 8px;
                 color: var(--grey10);
                 background-color: var(--brand2);
+                font-size: 16px;
+            }
+        }
+    }
+
+    @media only screen and (min-width: 768px) {
+    width: 100%;
+    height: 380px;
+    
+
+    .info-sem-logar{
+
+        width: 748px;
+        height: 261px;
+        .sub-herder-title{
+            font-size: 44px;
+            line-height: 56px;
+        }
+        .sub-header-subtitle{
+            font-weight: var(--heading-1-700);
+            font-size: 16px;
+        }
+        .div-buttons-sub-header{
+            gap: 30px;
+            flex-direction: row;
+
+            .button-sub-header{
+                width: 180px;
+                padding: 12px 28px;
 
             }
+        }
         }
     }
 `
