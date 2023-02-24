af5b0bee88ee4675a17eee36c2491f3cfa271279
SILVESTRE DAUFENBACH JUNIOR <98761413+silvestrejunior@users.noreply.github.com>
2023-02-24 00:02:59 -0300

    fix: SubHeader ajustado e bug resposivo arrumado

--- src/components/SubHeader/index.tsx
+++ src/components/SubHeader/index.tsx
@@ -1,24 +1,39 @@
+import { useProductContext } from "../../contexts/productContext";
 import { IProps } from "../../interfaces";
-import { SubHeaderDiv } from "./styles";
+import { InfosUserPage } from "../InfosUserPage";
+import { Container, SubHeaderDiv } from "./styles";
 
 
-const SubHeader: React.FC<IProps> = ({children}:IProps) => {
+const SubHeader: React.FC<IProps> = () => {
+   const { isLogged } = useProductContext();
+
   return (
-    <SubHeaderDiv>
-      {children?
-      (<>
-        {children}
-      </>)
+    <>
+      {isLogged=== true?
+
+      (<Container>
+        <div className="divBlue"></div>
+        <div>
+          <InfosUserPage
+            abrevName="J"
+            name="João"
+            description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+          />
+        </div>
+      </Container>)
       :
-      ( <div className="info-sem-logar">
-                <h1 className="sub-herder-title">Velocidade e experiência em um lugar feito para você</h1>
-                <p className="sub-header-subtitle">Um ambiente feito para você explorar o seu melhor</p>
-                <div className="div-buttons-sub-header">
-                    <button className="button-sub-header">Carros</button>
-                    <button className="button-sub-header">Motos</button>
-                </div>
-            </div> )}
-    </SubHeaderDiv>
+      (<SubHeaderDiv>
+          <div className="info-sem-logar">
+                  <h1 className="sub-herder-title">Velocidade e experiência em um lugar feito para você</h1>
+                  <p className="sub-header-subtitle">Um ambiente feito para você explorar o seu melhor</p>
+                  <div className="div-buttons-sub-header">
+                      <button className="button-sub-header">Carros</button>
+                      <button className="button-sub-header">Motos</button>
+                  </div>
+              </div> 
+
+      </SubHeaderDiv>)}
+    </>
   );
 };
 
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,17 @@
-import { CardPublished } from "../../components/CardPublished";
 import { CardVehicle } from "../../components/CardVehicles";
 import Carousel from "../../components/Carousel";
 import CarouselAuction from "../../components/CarouselAuction";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
+import SubHeader from "../../components/SubHeader";
 import { Vehicle } from "../../utils/data";
-import "./home.css";
+import "./index.css";
 
 const Home = () => {
   return (
     <div className="home-div">
       <Header></Header>
+      <SubHeader></SubHeader>
       <CarouselAuction></CarouselAuction>
 
       <Carousel
