f026a529ceaf7cef8e63bf6332b477a1419795d1
patrickcordeiroestudos <75341307+patrickcordeiro@users.noreply.github.com>
2023-02-27 15:40:06 -0300

    feat: Criando página e adicionando rota de login

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,14 @@
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+
+const Login = () => {
+  return (
+    <>
+      <Header />
+      <main>modal</main>
+      <Footer />
+    </>
+  );
+};
+
+export { Login };
